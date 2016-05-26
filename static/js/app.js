e325df211b6872e5e3a1179e6d77cabb9c6a9ccc
jbehuet <jbehuet@gmail.com>
2016-05-26 16:30:06 +0200

    Add router & semantic css

--- static/js/app.js
+++ static/js/app.js
@@ -1,33 +1,77 @@
-(function(){
+const {
+  Router,
+  Route,
+  IndexRoute
+} = ReactRouter;
 
-  class List extends React.Component {
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+          {/* Views will be rendered here */}
+          {this.props.children}
+        </div>
+    );
+  }
+}
 
-    constructor() {
-      super()
-      this.state = { todos: [] }
-    }
+class List extends React.Component {
 
-    componentDidMount(){
-      $.get( '/api/todos', function(data) {
-        this.setState({ todos: data })
-      }.bind(this));
-    }
-
-    render() {
-      var rows = [];
-      this.state.todos.forEach(function(todo, id){
-          rows.push(<li key={id}>{todo.description}</li>)
-      })
-      return (
-        <ul>
-          {rows}
-        </ul>
-      )
-    }
+  constructor() {
+    super()
+    this.state = { todos: [] }
   }
 
-  ReactDOM.render(
-    <List />,
-    document.getElementById('app')
-  );
-})();
+  componentDidMount(){
+    $.get( '/api/todos', function(data) {
+      this.setState({ todos: data })
+    }.bind(this));
+  }
+
+  render() {
+    var rows = [];
+    this.state.todos.forEach(function(todo, id){
+        rows.push(
+          <div className="item" key={id}>
+            <i className="large comment middle aligned icon"></i>
+            <div className="content">
+              <a className="header">{todo.description}</a>
+              <div className="description">#{todo.id}</div>
+            </div>
+          </div>)
+    })
+    return (
+      <div className="ui relaxed divided list">
+        {rows}
+      </div>
+    )
+  }
+}
+
+class About extends React.Component {
+  render() {
+    return (
+        <div>
+          <h1>About</h1>
+        </div>
+    );
+  }
+}
+
+class Routes extends React.Component {
+  render() {
+    return (
+        <Router>
+          <Route path="/" component={App}>
+            <IndexRoute component={List}/>
+            <Route path="/about" component={About}/>
+          </Route>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(
+   <Routes />,
+   document.getElementById('app')
+ );
